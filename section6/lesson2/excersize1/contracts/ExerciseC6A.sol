c924ce37c9ca1be8a24d6c7a07eb17021981691f
Daniel Escobar <escobard90@gmail.com>
2019-04-08 12:50:44 -0600

    section6 - feat: updated smart contract logic with multiple concensus algorythm logic, and comments

--- section6/lesson2/excersize1/contracts/ExerciseC6A.sol
+++ section6/lesson2/excersize1/contracts/ExerciseC6A.sol
@@ -6,19 +6,23 @@ contract ExerciseC6A {
     /*                                       DATA VARIABLES                                     */
     /********************************************************************************************/
 
+    // sets the variable to maintain the M-of-N ratio
+    uint constant M = 2;
 
     struct UserProfile {
         bool isRegistered;
         bool isAdmin;
     }
 
+    // stores the addresses necessary for the multiParty concensus
+    // this syntax creates a new address' array, for use within the multiParty concensus logic
+    address[] multiCalls = new address[](0);
+
     address private contractOwner;                  // Account used to deploy contract
     mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
 
     // establishes weather the contract is operating or not
     bool private operational = false;
-
-
 
     /********************************************************************************************/
     /*                                       EVENT DEFINITIONS                                  */
@@ -85,6 +89,7 @@ contract ExerciseC6A {
                                     bool isAdmin
                                 )
                                 external
+                                isOperational
                                 requireContractOwner
     {
         require(!userProfiles[account].isRegistered, "User is already registered.");
@@ -97,13 +102,53 @@ contract ExerciseC6A {
 
     // sets contract operations on / off, while off, all write transactions will fail
     function setOperatingStatus(bool mode) external requireContractOwner {
-        operational = mode;
+
+        // best practice with ether, we want functions to fail fast to avoid unecessary costs
+
+        // prevents function calls that attempt to change the operational boolean to false, when already false
+        require(mode != operational, "New mode must be different from existing mode");
+
+        // checks if the user's address has been registered as an admin or not, check registerUser() for functionality specs
+        require(userProfiles[msg.sender].isAdmin, "Caller is not an admin");
+
+        // ensures that there is no double voting, ensures no admin can vote twice on the same multi-concenus transaction
+        bool isDuplicate = false;
+
+        // loops through the multicalls array, if address exists in multi call array, set the isDuplicate flag to true
+        for (uint c=0; c < multiCalls.length; c++){
+
+            // sets the isDuplicate boolean to true, if the caller's vote has already been registered
+            if(multiCalls[c] == msg.sender){
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        // requires that the isDuplicate flag is false, to ensure no duplicate votes are registered
+        // remember, failing fast is best practice with ethereum
+        require(!isDuplicate, "Caller's vote has already been registered");
+        
+        // if none of the fail conditions are met, push the address into the multiCalls address array
+        multiCalls.push(msg.sender);
+
+        // if the multiCalls array length is the same as the established M-of-N number, executes transaction logic
+        // this ensures that the logic in the if statement is ONLY called if 3 admin's have voted / been approved by the function's logic
+        if(multiCalls.length >= M){
+            operational = mode;
+
+            // re-initializes the multiCalls array, ensuring that the instance of the array is destroyed
+            // every time concesus is met
+            multiCalls = new address[](0);
+        }
+
+        
     }
 
     function getOperational() public view returns(bool){
         return operational;
     }
 
+    // very important to test this locally to ensure this logic is working
     modifier isOperational(){
         // second argument throws an error if require condition is not met, neat!
         require(operational, "Contract is currently not operational");
